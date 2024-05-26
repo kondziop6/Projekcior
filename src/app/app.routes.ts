4c86387686a50b8d4c395c89215378fd6935baba
Krzysztof <krzysztofp730@outlook.com>
2024-05-26 14:52:07 +0200

    Dodano inventory to ścieżek

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,28 +3,29 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LogoutComponent } from './logout/logout.component';
 import { LoginComponent } from './login/login.component';
-import { HeaderComponent} from './header/header.component';
+import { HeaderComponent } from './header/header.component';
 import { ChromaComponent } from './chroma/chroma.component';
 import { Chroma2Component } from './chroma2/chroma2.component';
 import { DangerZoneComponent } from './danger-zone/danger-zone.component';
 import { FalchionComponent } from './falchion/falchion.component';
 import { ShadowComponent } from './shadow/shadow.component';
-
+import { InventoryComponent } from './inventory/inventory.component';
 
 export const routes: Routes = [
-  { path: 'register', component: RegisterComponent },
-  { path: 'logout', component: LogoutComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'chroma', component: ChromaComponent },
-  { path: 'chroma2', component: Chroma2Component },
-  { path: 'danger-zone', component: DangerZoneComponent },
-  { path: 'falchion', component: FalchionComponent },
-  { path: 'shadow', component: ShadowComponent },
-  { path: '', component: HeaderComponent }
+    { path: 'register', component: RegisterComponent },
+    { path: 'logout', component: LogoutComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'chroma', component: ChromaComponent },
+    { path: 'chroma2', component: Chroma2Component },
+    { path: 'danger-zone', component: DangerZoneComponent },
+    { path: 'falchion', component: FalchionComponent },
+    { path: 'shadow', component: ShadowComponent },
+    { path: 'inventory', component: InventoryComponent },
+    { path: '', component: HeaderComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
